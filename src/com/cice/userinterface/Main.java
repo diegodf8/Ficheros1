aa77ce5b00e4234c5a07338261faa4277c32ccab
Diego Dominguez <diegodf8@gmail.com>
2018-03-07 21:05:44 +0100

    Commit inicial de ficheros

--- src/com/cice/userinterface/Main.java
+++ src/com/cice/userinterface/Main.java
@@ -1,16 +1,23 @@
 package com.cice.userinterface;
 
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 
 public class Main {
 
     public static void main(String[] args) {
 
         try {
-            FileReader fileReader = new FileReader("Texto.txt");
+            FileReader fileReader = new FileReader("C:\\Users\\Diego_Yako\\Desktop\\Text.txt");
+            BufferedReader bufferReader = new BufferedReader(fileReader);
+            String cadena = bufferReader.readLine();
+            System.out.println(cadena);
         }catch (FileNotFoundException ex){
             ex.printStackTrace();
+        }catch (IOException ex2){
+            ex2.printStackTrace();
         }
 
     }
